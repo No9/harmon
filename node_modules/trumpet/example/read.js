ce912a0bec3daaf395372cca2e5ab80823f2574f
no9 <antonwhalley@whalley@vodafone.ie>
2014-04-16 23:17:53 +0100

    remove node_modules

--- node_modules/trumpet/example/read.js
+++
@@ -1,7 +0,0 @@
-var trumpet = require('../');
-var tr = trumpet();
-
-tr.select('.msg').createReadStream().pipe(process.stdout);
-
-var fs = require('fs');
-fs.createReadStream(__dirname + '/html/read.html').pipe(tr);
