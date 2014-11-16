1a521bbcab6a731c91f95efcb6c1af502db5d72f
Guy Paddock <guy.paddock@redbottledesign.com>
2014-11-15 22:39:53 -0500

    - Clean-up and reorganize code for readability. - Introduce checks to ensure that only HTML responses are modified.

--- index.js
+++ index.js
@@ -1,69 +1,94 @@
 var trumpet = require('trumpet');
 
-module.exports = function harmon(reqselectors, resselectors, htmlonly) {
+module.exports = function harmonBinary(reqSelectors, resSelectors) {
+	var _reqSelectors = reqSelectors || [];
+	var _resSelectors = resSelectors || [];
 
-	var reqselectors = reqselectors || [];
-	var resselectors = resselectors || [];
-
-	return function harmon(req, res, next) {
-		
-		var ignore = false;
-
-		if(htmlonly){
-			if((req.url.toLowerCase().indexOf('.js', req.url.length - 3) !== -1) || 
-			   (req.url.toLowerCase().indexOf('.css', req.url.length - 4) !== -1)) {
-			   	ignore = true;
-			}
-		}
-
-		if(ignore == false) {
-			if (reqselectors.length) {
-				var reqtr = trumpet();
-				for(var i = 0; i < reqselectors.length; i++){
-					reqtr.select(reqselectors[i].query, reqselectors[i].func);
-				}
-				
-				req.on('data', function(data){
-					reqtr.write(data);
-				});
-			}
-
-			if (resselectors.length) {
-				var tr = trumpet();
-				
-				for(var i = 0; i < resselectors.length; i++){
-					tr.selectAll(resselectors[i].query, resselectors[i].func);
-				}
-
-				var _write = res.write;
-				var _end = res.end;
-	            var _writeHead = res.writeHead;
-
-				res.write = function (data, encoding) {
-					tr.write(data, encoding);
-				};
-
-				res.end = function (data, encoding) {
-					tr.end(data, encoding);
-				};
-
-				tr.on('end', function () {
-					_end.call(res);
-				});
-
-				tr.on('data', function (buf) {
-					_write.call(res, buf);
-				});
-
-	            res.writeHead = function (code, headers) {
-	            	
-	            	//console.log(res.getHeader('content-type'))
-	                res.removeHeader('Content-Length');
-	                if (headers) { delete headers['content-length']; }
-	                _writeHead.apply(res, arguments);
-	            };
-			}
-		}
-		next();
+	function prepareRequestSelectors(req, res) {
+    var tr = trumpet();
+  
+    prepareSelectors(tr, _reqSelectors, req, res);
+    
+    req.on('data', function(data) {
+      tr.write(data);
+    });
 	}
-}
+	
+	function prepareResponseSelectors(req, res) {
+    var tr          = trumpet();
+    var _write      = res.write;
+    var _end        = res.end;
+    var _writeHead  = res.writeHead;
+
+    prepareSelectors(tr, _resSelectors, req, res);
+
+    // Assume response is binary by default
+    res.isHtml = false;
+    
+    res.writeHead = function (code, headers) {
+      var contentType = this.getHeader('content-type');
+      
+      /* Sniff out the content-type header.
+       * If the response is HTML, we're safe to modify it.
+       */
+      if (contentType.indexOf('text/html') == 0) {
+        res.isHtml = true;
+
+        // Strip off the content length since it will change.
+        res.removeHeader('Content-Length');
+
+        if (headers) {
+          delete headers['content-length'];
+        }
+      }
+      
+      _writeHead.apply(res, arguments);
+    };
+    
+    res.write = function (data, encoding) {
+      // Only run data through trumpet if we have HTML
+      if (res.isHtml) {
+        tr.write(data, encoding);
+      }
+      
+      else {
+        _write.apply(res, arguments);
+      }
+    };
+
+    tr.on('data', function (buf) {
+      _write.call(res, buf);
+    });
+
+    res.end = function (data, encoding) {
+      tr.end(data, encoding);
+    };
+
+    tr.on('end', function () {
+      _end.call(res);
+    });
+	}
+
+	function prepareSelectors(tr, selectors, req, res) {
+    for (var i = 0; i < selectors.length; i++) {
+      var callback         = selectors[i].func;
+      var callbackInvoker  = function(element) {
+        callback(element, req, res);
+      }; 
+      
+      tr.selectAll(selectors[i].query, callbackInvoker);
+    }
+  }
+	
+	return function harmonBinary(req, res, next) {
+		if (_reqSelectors.length) {
+		  prepareRequestSelectors(req, res);
+		}
+
+		if (_resSelectors.length) {
+		  prepareResponseSelectors(req, res);
+		}
+		
+		next();
+	};
+};
