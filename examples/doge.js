bf3583cae34635d73cfe488b74bae112f5eecd7e
No9 <mrcowsy@gmail.com>
2015-04-04 20:54:37 +0100

    removed extra server in sample

--- examples/doge.js
+++ examples/doge.js
@@ -60,9 +60,3 @@ app.use(
 );
 
 http.createServer(app).listen(8000);
-
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<html><head></head><body><div class="a">Nodejitsu Http Proxy</div><div class="b">&amp; Frames</div></body></html>');
-  res.end();
-}).listen(9000); 
