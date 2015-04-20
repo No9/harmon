552184cd3a90657491074893f91bf25d8bf422e5
Jhorlin <jhorlin@gmail.com>
2015-04-20 02:05:21 -0400

    When creating a harmon html only proxy  and the content is not html but is gziped the data is not written through gunzip but the gzip Content-Encoding has been removed. The fix is to test for res.isHtml first then check for gzip only if res.isHtml is true. This way non html is proxied correclty with zipped content and the correct Content-Encoding header

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ module.exports = function harmonBinary(reqSelectors, resSelectors, htmlOnly) {
 
   function prepareRequestSelectors(req, res) {
     var tr = trumpet();
-  
+
     prepareSelectors(tr, _reqSelectors, req, res);
-    
+
     req.on('data', function(data) {
       tr.write(data);
     });
   }
-    
+
   function prepareResponseSelectors(req, res) {
     var tr          = trumpet();
     var _write      = res.write;
@@ -27,29 +27,14 @@ module.exports = function harmonBinary(reqSelectors, resSelectors, htmlOnly) {
 
     // Assume response is binary by default
     res.isHtml = false;
-    
+
     // Assume response is uncompressed by default
     res.isGziped = false;
 
     res.writeHead = function (code, headers) {
       var contentType = this.getHeader('content-type');
-      
+
       var contentEncoding = this.getHeader('content-encoding');
-
-
-      /* Sniff out the content-type header.
-       * If the response is Gziped, we're have to gunzip content before and ungzip content after.
-       */
-      if (contentEncoding && contentEncoding.toLowerCase() == 'gzip') {
-        res.isGziped = true;
-
-        // Strip off the content encoding since it will change.
-        res.removeHeader('Content-Encoding');
-
-        if (headers) {
-          delete headers['content-encoding'];
-        }
-      }
 
       /* Sniff out the content-type header.
        * If the response is HTML, we're safe to modify it.
@@ -64,10 +49,24 @@ module.exports = function harmonBinary(reqSelectors, resSelectors, htmlOnly) {
           delete headers['content-length'];
         }
       }
-      
+
+      /* Sniff out the content-type header.
+       * If the response is Gziped, we're have to gunzip content before and ungzip content after.
+       */
+      if (res.isHtml && contentEncoding && contentEncoding.toLowerCase() == 'gzip') {
+          res.isGziped = true;
+
+          // Strip off the content encoding since it will change.
+          res.removeHeader('Content-Encoding');
+
+          if (headers) {
+              delete headers['content-encoding'];
+          }
+     }
+
       _writeHead.apply(res, arguments);
     };
-    
+
     res.write = function (data, encoding) {
       // Only run data through trumpet if we have HTML
       if (res.isHtml) {
@@ -117,7 +116,7 @@ module.exports = function harmonBinary(reqSelectors, resSelectors, htmlOnly) {
       })(selectors[i].func, req, res);
     }
   }
-    
+
   return function harmonBinary(req, res, next) {
     var ignore = false;
 
