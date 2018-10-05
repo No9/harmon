56fe18cd01885ebbc0d6e4fa6cf44cb55a0cd75f
Grant Levene <glevene@arbor.net>
2018-10-05 18:44:49 -0400

    bump http-proxy version & fix failing test

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ module.exports = function harmonBinary(reqSelectors, resSelectors, htmlOnly) {
     res.isHtml = function () {
       if (res._isHtml === undefined) {
         var contentType = res.getHeader('content-type') || '';
-        res._isHtml = contentType.startsWith('text/html');
+        res._isHtml = contentType.indexOf('text/html') === 0;
       }
 
       return res._isHtml;
@@ -118,7 +118,6 @@ module.exports = function harmonBinary(reqSelectors, resSelectors, htmlOnly) {
 
   return function harmonBinary(req, res, next) {
     var ignore = false;
-
 
     if (_htmlOnly) {
       var lowercaseUrl = req.url.toLowerCase();
